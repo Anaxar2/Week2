08c6ddafa28c9336756523d1c0b350bfc5ee8adf
Anaxar2 <165289717+Anaxar2@users.noreply.github.com>
2024-04-03 15:17:18 +0100

    First & Third person Camera Added.

--- Week 2 Day 1-2/Assets/Scripts/FollowPlayer.cs
+++ Week 2 Day 1-2/Assets/Scripts/FollowPlayer.cs
@@ -3,20 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
-    
-{ public GameObject Player;// Created component to add Player to script.
-  private Vector3 offset = new Vector3(0, 8, -10); 
-    
+
+{
+    public GameObject Player;// Created component to add Player to script.
+    public Vector3 thirdPersonOffset = new Vector3(0, 8, -10);
+    public Vector3 firstPersonOffset;
+    private Vector3 currentOffset;
+    private bool isfirstPerson = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
 
-}
+    }
 
-// Update is called once per frame
-void LateUpdate()
+    // Update is called once per frame
+    void LateUpdate()
     {
-        transform.position = Player.transform.position + offset;
+        if (isfirstPerson) currentOffset = Player.transform.position + firstPersonOffset;
+        else currentOffset = Player.transform.position + thirdPersonOffset;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!isfirstPerson) isfirstPerson = true;
+            else isfirstPerson = false;
+        }
+
+
+        transform.position = currentOffset;
+
     }
 }
--- Week 2 Day 1-2/Assets/Scripts/PlayerController.cs
+++ Week 2 Day 1-2/Assets/Scripts/PlayerController.cs
@@ -6,8 +6,11 @@ using UnityEngine.Jobs;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f; // Created component named speed with a value of 5.
-   
+  public float speed = 5.0f; // Created component named speed with a value of 5.
+    public float turnSpeed;
+    public float horizontalInput;
+    public float verticalInput;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,13 +20,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the vechicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-
-        //Move left and Right
-        transform.Translate(Input.GetAxis("Horizontal"), 0, 0);
 
        
+        horizontalInput = Input.GetAxis("Horizontal");
+       
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);  // Moves the vechicle forward
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+
+        //transform.Translate(Input.GetAxis("Horizontal"), 0, 0); //Move left and Right
+
+
 
     }
 }
